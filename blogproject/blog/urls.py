3892192d58e48119eb69fe8f30434dc072561e3a
kbsat <kbsat@naver.com>
2020-07-15 12:46:27 +0900

    URL의 분리

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blogapp.views
 import portfolio.views
 from django.conf import settings # 외워야해
@@ -8,10 +8,7 @@ from django.conf.urls.static import static # 외워야해
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blogapp.views.index, name="index"),
-    path('detail/<int:blog_id>',blogapp.views.detail,name="detail"),
-    path('new/',blogapp.views.new,name = "new"),
-    path('create/',blogapp.views.create,name="create"),
-    path('delete/<int:blog_id>',blogapp.views.delete,name="delete"),
+    path('blog/',include('blogapp.urls')),
     path('portfolio/',portfolio.views.portfolio,name="portfolio"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 외워야해
 # 이런식으로 적어도 됨
